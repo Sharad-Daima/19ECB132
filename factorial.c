fd41eb69709a41c81719c62ed3b0396fc43ed823
Sharad-Daima <101248729+Sharad-Daima@users.noreply.github.com>
2022-04-04 21:17:48 +0530

    Create factorial.c

---
+++ factorial.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int main(){
+    int a, b=1, c;
+    printf("Enter an integer: "); scanf("%d",&a);
+    
+    if(a==0){
+        printf("1");
+    }
+    
+    else if (a>=10){
+        printf("\nToo big a number");
+    }
+    
+    else if (a>0 && a<10){
+        for (c=1; c<=a; c++)
+        {
+            b*=c;
+        }
+        printf("\n%d",b);
+    }
+    
+    else if(a<0){
+        printf("\nNegative Number");
+    }
+}
