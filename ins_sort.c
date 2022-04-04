e0646da480af951af00a9a47a97b727e2a7d7299
Sharad-Daima <101248729+Sharad-Daima@users.noreply.github.com>
2022-04-04 21:16:31 +0530

    Create ins_sort.c

---
+++ ins_sort.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main(){
+	int a, b, c = 0, array[5] = {5,3,4,2,1}; //hard coded
+	
+	for (a=0; a<5; a++){
+		for (b=a+1; b<5; b++){
+			if(array[a]> array[b]){
+				c = array[a];
+				array[a] = array[b];
+				array[b] = c;
+			}
+		}
+	}
+	printf("\nSorted array is: \n");
+	for (a=0; a<5; a++){
+		printf("\t%d", array[a]);
+	}
+}
