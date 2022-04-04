8a6336de84496d8ef11aba0f2743f66852f9d9f5
Sharad-Daima <101248729+Sharad-Daima@users.noreply.github.com>
2022-04-04 21:26:53 +0530

    Create fibonacci_norec.c
    Fibonacci series up to n elements, without recursions.

---
+++ fibonacci_norec.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+int main(){
+  int a=0,b=1,c,x,y;
+  printf("Enter the range: "); scanf("%d", &y);
+  printf("%d\t%d", a, b);
+  for(x=0; x<y-2; x++){
+    c = a+b;
+    printf("\t%d", c);
+    a = b;
+    b = c;
+  }
+}
