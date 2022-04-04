a67b347a2dcefd1d65a210372ff47dc010cc88a9
Sharad-Daima <101248729+Sharad-Daima@users.noreply.github.com>
2022-04-04 21:36:40 +0530

    Create fibonacci_rec.c
    Fibonacci series to print n elements, with recursion.

---
+++ fibonacci_rec.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int fibonacci(int n){
+  static int x=0, y=1, z;
+  if(n>0){
+    z = x+y;
+    printf("%d ", z);
+    x = y;
+    y = z;
+    fibonacci(n-1);
+  }
+}
+
+int main(){
+  int a;
+  printf("Enter the range: "); scanf("%d", &a);
+  printf("Fibonacci sequence: \n");
+  printf("0 1 ");
+  fibonacci(a-2);
+}
