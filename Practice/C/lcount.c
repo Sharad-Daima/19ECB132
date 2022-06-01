07ef0f54441f1355e7565512b247c14acc96af11
Sharad-Daima <101248729+Sharad-Daima@users.noreply.github.com>
2022-06-01 09:12:56 +0530

    Update lcount.c

--- Practice/C/lcount.c
+++ Practice/C/lcount.c
@@ -2,15 +2,21 @@
 #include<string.h>
 
 int main(void){
-  char sen[];
+  char sen[10];
   int a, b=0, c;
-  
+  scanf("%s", sen);
   c = strlen(sen);
-  
+
   for(a=0; a<c; a++){
-     if(sen[a]!==' '){
-        b++
+     if(sen[a]!=' '){
+        b++;
      }
   }
-  printf("%d", &b);
+  printf("%d", b);
 }  
+/*
+Input:
+Hello
+Output:
+5 [Returns the number of letters in the word]
+*/
